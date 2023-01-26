cf35f82e62b4fcaa17ff89f426d5e85bbe179ab5
Gervasio Artur <gervasioartur@outlook.com.br>
2023-01-25 23:37:49 -0300

    feat: add mysql connector java

--- src/main/java/com/gervasioartur/todosimple/TodosimpleApplication.java
+++ src/main/java/com/gervasioartur/todosimple/TodosimpleApplication.java
@@ -2,8 +2,9 @@ package com.gervasioartur.todosimple;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.sql.init.SqlInitializationAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SqlInitializationAutoConfiguration.class)
 public class TodosimpleApplication {
 
 	public static void main(String[] args) {
