7dd66e5e2007932db985a2b94aae2662849838cb
Gervasio Artur <gervasioartur@outlook.com.br>
2023-02-07 19:33:21 -0300

    feat: adding TaskController

---
+++ src/main/java/com/gervasioartur/todosimple/controllers/TaskController.java
@@ -0,0 +1,51 @@
+package com.gervasioartur.todosimple.controllers;
+
+import com.gervasioartur.todosimple.models.Task;
+import com.gervasioartur.todosimple.services.TaskService;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/task")
+@Validated
+public class TaskController {
+    @Autowired
+    private TaskService taskService;
+
+    public ResponseEntity<Task> findById(@PathVariable Long id){
+        Task task = this.taskService.findById(id);
+        return ResponseEntity.ok(task);
+    }
+
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<List<Task>> findByUserId(@PathVariable Long userId ){
+        List<Task> taskList =  this.taskService.findAllByUserId(userId);
+        return ResponseEntity.ok().body(taskList);
+    }
+
+    public ResponseEntity<Void> create (@Valid @RequestBody Task task){
+        this.taskService.create(task);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(task.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    public ResponseEntity<Void> update(@Valid @RequestBody Task task, @PathVariable Long id){
+        task.setId(id);
+        this.taskService.update(task);
+        return ResponseEntity.noContent().build();
+    }
+    public ResponseEntity<Void> delete(@PathVariable Long id ){
+        this.taskService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
+
